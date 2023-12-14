3b0f55d14e89325fad5b082885face1fe5a15c15
embarq <embarq.dev@gmail.com>
2023-12-14 15:58:09 +0900

    add and configure prisma and prisma accelerate

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,29 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const app = new Hono().basePath('/api')
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+app.get('/', (c) =>
+  c.json({
+    message: 'Hello  Hono!',
+    env: process.env.DATABASE_URL?.slice(0, 21),
+  }),
+)
+
+// dev-server
+if (process.env.NODE_ENV === 'development') {
+  import('@hono/node-server').then((m) => {
+    m.serve(app, (info) => {
+      console.log(`Listening on http://localhost:${info.port}`)
+    })
+  })
+}
 
 export const config = {
   runtime: 'edge',
 }
-
-const app = new Hono().basePath('/api')
-
-app.get('/', (c) => c.json({ message: 'Hello Hono!' }))
 
 export default handle(app)
